d528c31ee33667bc10cecd2788666aa4c92d6641
giulia97-w <72927421+giulia97-w@users.noreply.github.com>
2023-03-15 11:43:11 +0100

    Update HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import java.util.logging.Logger;
+
 public class HelloWorld {
+    private static final Logger logger = Logger.getLogger(HelloWorld.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        logger.info("Hello, World!");
     }
 }
